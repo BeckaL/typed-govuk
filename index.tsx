3121432779f7238688787d91e9579f962ee4b68a
Becka Lelew <becka.lelew@digital.cabinet-office.gov.uk>
2022-12-05 14:42:17 +0000

    Add koa server
    Serves basic information about topical events

--- index.tsx
+++ index.tsx
@@ -1,16 +1,30 @@
 import * as React from 'react'
-import {Schema} from './topical-event'
-import {renderToStaticMarkup} from "react-dom/server";
+import { Schema as TopicalEventSchema } from './topical-event'
+import { renderToStaticMarkup } from "react-dom/server";
+import Koa from 'koa';
+import KoaRouter from 'koa-router';
 
-const main = async () => {
-    const schema: Schema = await fetch("https://www.gov.uk/api/content/government/topical-events/budget-2021")
+const app = new Koa();
+const router = new KoaRouter();
+
+const getTopicalEvent = async (name: string) => {
+    const contentItem: TopicalEventSchema = await fetch(`https://www.gov.uk/api/content/government/topical-events/${name}`)
         .then(resp => resp.json());
-
-    return (
-        <p>{schema.public_updated_at}</p>
+    return renderToStaticMarkup(
+        <div>
+          <h1>{contentItem.title}</h1>
+          <p>Updated at: {contentItem.public_updated_at}</p>
+        </div>
     )
-};
+}
 
-main().then(page => console.log(renderToStaticMarkup(page)))
+router.get('/', (ctx, _) => {
+    ctx.body = `Go to a gov.uk base path i.e /government/topical-events/budget-2021 to see something rendered`;
+}).get('/government/topical-events/:topical_event_name', (ctx, _) => {
+    return getTopicalEvent(ctx.params.topical_event_name).then(html => {ctx.body = html})
+}
+);
 
-// <p>Hello world</p>;
+app.use(router.routes())
+
+app.listen(3000)
