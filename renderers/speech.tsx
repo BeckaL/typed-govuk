a8711cfa738ebf5b54f985f5ad9efac398289d53
Becka Lelew <becka.lelew@digital.cabinet-office.gov.uk>
2022-12-06 16:28:53 +0000

    Add rendering of govspeak body

--- renderers/speech.tsx
+++ renderers/speech.tsx
@@ -2,13 +2,23 @@ import { Schema as SpeechSchema } from '../compiled-schemas/speech'
 import { title } from "./components/title.js"
 import * as React from 'react'
 import { renderPage } from './renderPage.js';
+import { renderGovspeakElem } from './components/govspeak.js'
 
 export const renderSpeech = (contentItem: SpeechSchema) => {
   return renderPage((
     <div>
       {title(contentItem.title, toTitleCase(contentItem.document_type))}
       <div className = "gem-c-lead-paragraph"><p>{contentItem.title}</p></div>
-    </div>
+
+      <div class="govuk-grid-row">
+        <div class="govuk-grid-column-two-thirds">
+          <div class="content-bottom-margin"></div>
+            <div class="responsive-bottom-margin">
+                {renderGovspeakElem(contentItem.details.body)}
+            </div>
+          </div>
+        </div>
+      </div>
   ))
 }
 
