d2e35a83e1903d642dddaaed08d48cae86ef8990
Becka Lelew <becka.lelew@digital.cabinet-office.gov.uk>
2022-12-06 14:01:42 +0000

    Introduce locale file and render footer from this data

--- renderers/components/footer.tsx
+++ renderers/components/footer.tsx
@@ -1,33 +1,48 @@
-import * as React from 'react'
+import * as React from 'react';
+import { parse } from 'yaml';
+import fs from 'fs';
 
-export const footer = (navLinks: NavigationLink[] = [topics, governmentActivity]) => {
+type Item = {
+    label: String,
+    href: String
+}
+
+type NavigationLink = {
+    label: String,
+    menu_contents: Item[]
+}
+
+const getNavLinks = (): NavigationLink[] => {
+    const file = fs.readFileSync('./data/en.yml', 'utf8');
+    return parse(file).en.components.layout_super_navigation_header.navigation_links as NavigationLink[]
+}
+
+export const footer = () => {
     return <div className="govuk-width-container">
                <div className="govuk-footer__navigation" data-module="gem-track-click" data-track-links-only>
-                   { console.log(navLinks) }
-                   { navigationLinksDiv(navLinks) }
+                   { navigationLinksDiv( getNavLinks()) }
                </div>
            </div>
 }
 
 const navigationLinksDiv = (links: NavigationLink[]) => {
-    return links.map(l => 
-        <div className={ `${columnWidthClass(l.column)}  govuk-!-display-none-print` }>
-            <h2 className="govuk-footer__heading govuk-heading-m">{l.title}</h2>
-            <ul className={ listClasses(l.column) }>
-                { l.items.map( (i: Item ) => 
+    return links.map( l => 
+        <div className={ `${columnWidthClass(columnNo(l.label))}  govuk-!-display-none-print` }>
+            <h2 className="govuk-footer__heading govuk-heading-m">{l.label}</h2>
+            <ul className={ listClasses(columnNo(l.label)) }>
+                { l.menu_contents.map( (i: Item ) => 
                     <li className="govuk-footer__list-item">
-                        <a href={ i.href }>{ i.text }</a>
+                        <a href={ i.href }>{ i.label }</a>
                     </li>
                 ) }
             </ul>
         </div>
-)
+        )
 }
 
-const listClasses = (noOfColumns: number) => {
-    return `govuk-footer__list govuk-footer__list--columns-${noOfColumns}}`
-}
+const columnNo = (title: String): number => (title === "Topics")? 2 : 1
 
+const listClasses = (noOfColumns: number): String => `govuk-footer__list govuk-footer__list--columns-${noOfColumns}}`
 
 const columnWidthClass= (noOfColumns: number): string => {
     switch (noOfColumns) {
@@ -38,129 +53,4 @@ const columnWidthClass= (noOfColumns: number): string => {
         default:
             return "govuk-grid-column-one-third"
     }
-}
-
-type Item = {
-    text: String,
-    href: String
-}
-
-type NavigationLink = {
-    title: String,
-    column: number,
-    items: Item[]
-}
-
-const topics: NavigationLink = {
-    title: "Topics",
-    column: 2,
-    items: [
-        {
-            text: "Benefits",
-            href: "/browse/benefits"
-        },
-        {
-            text: "Births, death, marriages and care",
-            href: "/browse/births-deaths-marriages"
-        },
-        {
-            text: "Business and self-employed",
-            href: "/browse/business"
-        },
-        {
-            text: "Childcare and parenting",
-            href: "/browse/childcare-parenting"
-        },
-        {
-            text: "Citizenship and living in the UK",
-            href: "/browse/citizenship"
-        },
-        {
-            text: "Cost of living support",
-            href: "/cost-of-living"
-        },
-        {
-            text: "Crime, justice and the law",
-            href: "/browse/justice"
-        },
-        {
-            text: "Disabled people",
-            href: "/browse/disabilities"
-        },
-        {
-            text: "Driving and transport",
-            href: "/browse/driving"
-        },
-        {
-            text: "Education and learning",
-            href: "/browse/education"
-        },
-        {
-            text: "Employing people",
-            href: "/browse/employing-people"
-        },
-        {
-            text: "Environment and countryside",
-            href: "/browse/environment-countryside"
-        },
-        {
-            text: "Housing and local services",
-            href: "/browse/housing-local-services"
-        },
-        {
-            text: "Money and tax",
-            href: "/browse/tax"
-        },
-        {
-            text: "Passports, travel and living abroad",
-            href: "/browse/abroad"
-        },
-        {
-            text: "Visas and immigration",
-            href: "/browse/visas-immigration"
-        },
-        {
-            text: "Working, jobs and pensions",
-            href: "/browse/working"
-        }]
-}
-
-const governmentActivity: NavigationLink = {
-    title: "Government activity",
-    column: 1,
-    items: [
-        {
-            text: "Departments",
-            href: "/government/organisations"
-        },
-        {
-            text: "News",
-            href: "/search/news-and-communications"
-        },
-        {
-            text: "Guidance and regulation",
-            href: "/search/guidance-and-regulation"
-        },
-        {
-            text: "Research and statistics",
-            href: "/search/research-and-statistics"
-        },
-        {
-            text: "Policy papers and consultations",
-            href: "/search/policy-papers-and-consultations"
-        },
-        {
-            text: "Transparency",
-            href: "/search/transparency-and-freedom-of-information-releases"
-        },
-        {
-            text: "How government works",
-            href: "/government/how-government-works"
-        },
-        {
-            text: "Get involved",
-            href: "/government/get-involved"
-        }
-
-    ]
 }
