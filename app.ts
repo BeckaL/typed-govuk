1bcbb6c82298cc1466fc37d44705dc3858478470
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2022-12-06 12:33:13 +0000

    Install types for koa
    This means that app.ts typechecks in my editor. It also means we can clean up a bit of type noise (because typescript can infer that ctx is a Context).
    
    I've also moved the things that aren't necessary for running the server into devDependencies.

--- app.ts
+++ app.ts
@@ -1,19 +1,18 @@
 import { renderBasedOnSchema, renderIndex } from './renderer.js';
 import Koa from 'koa';
 import KoaRouter from 'koa-router';
-import { Context } from 'koa';
 
 const app = new Koa();
 const router = new KoaRouter();
 
 
-router.get('/', (ctx: Context, _) => {
+router.get('/', (ctx) => {
     ctx.body = renderIndex();
-}).get('/government/topical-events/:topical_event_name', (ctx: Context, _) => {
+}).get('/government/topical-events/:topical_event_name', (ctx) => {
     return renderBasedOnSchema(`/government/topical-events/${ctx.params.topical_event_name}`).then(html => {ctx.body = html})
-}).get('/world/:location/news', (ctx: Context, _) => {
+}).get('/world/:location/news', (ctx) => {
     return renderBasedOnSchema(`/world/${ctx.params.location}/news`).then(html => {ctx.body = html})
-}).get('/government/case-studies/:case_study_name', (ctx: Context, _) => {
+}).get('/government/case-studies/:case_study_name', (ctx) => {
     return renderBasedOnSchema(`/government/case-studies/${ctx.params.case_study_name}`).then(html => {ctx.body = html})
 });
 
