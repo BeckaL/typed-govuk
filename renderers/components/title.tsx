9adaba360397766ae3cda750b14694aedbe8decf
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2022-12-06 17:27:20 +0000

    Fix react warnings
    Two flavours of warning:
    
    1) The classic of having to use "className" instead of "class" because it's the
    DOM property not the HTML attribute name
    
    2) React is fussy about lists of
    children if the children don't have keys. This isn't a problem for us because
    we're rendering everything statically, so it doesn't matter if react can't track
    which element is which, but it still complains. Adding React.Children.toArray fixes
    this.

--- renderers/components/title.tsx
+++ renderers/components/title.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 
 export const title = (t: String, documentType: String) => {
     return <div className="gem-c-title govuk-!-margin-top-8 govuk-!-margin-bottom-8">
-              <span class="govuk-caption-xl gem-c-title__context">{documentType}</span>
+              <span className="govuk-caption-xl gem-c-title__context">{documentType}</span>
                <h1 className="govuk-heading-xl gem-c-title__text">{t}</h1>
            </div>
 }
