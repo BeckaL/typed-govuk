cee3cbad25a80c0b2f65380362cab0ef1d85cbdb
Becka Lelew <becka.lelew@digital.cabinet-office.gov.uk>
2022-12-07 09:34:58 +0000

    Add css from government frontend

--- renderers/renderPage.tsx
+++ renderers/renderPage.tsx
@@ -7,11 +7,13 @@ import { renderToStaticMarkup } from "react-dom/server";
 export const renderPage = (page) => {
     const applicationStylesheet = "https://www.gov.uk/assets/static/application-2ff816c36affa8947857ec173cb86a848ca6559ef267a9245df61d2aa326f2df.css"
     const collectionsStylesheet = "https://www.gov.uk/assets/collections/application-07cc5dfda62258efd4b102a4e531e8d833f12007897649b6ef1045f9848e2d08.css"
+    const frontendStylesheet = "https://www.gov.uk/assets/government-frontend/application-814f34c2ef20f2022c151c65111732a389100dc06a253a0b5464231377c60211.css"
       return renderToStaticMarkup(
           <html>
             <head>
                <link rel="stylesheet" href={applicationStylesheet} media="all"/>
                <link rel="stylesheet" href={collectionsStylesheet} media="all"/>
+               <link rel="stylesheet" href={frontendStylesheet} media="all"/>
              </head>
              <body className="gem-c-layout-for-public govuk-template__body">
               { header() }
