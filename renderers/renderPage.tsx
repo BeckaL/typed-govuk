6452d1c7e9475080d7ba0e50d3a7b787c5bd4694
Becka Lelew <becka.lelew@digital.cabinet-office.gov.uk>
2022-12-08 10:11:55 +0000

    Add js scripts
    These should in theory make the navbar drop downs work. Not currently working though, will have to have a look at why

--- renderers/renderPage.tsx
+++ renderers/renderPage.tsx
@@ -8,13 +8,16 @@ export const renderPage = (page) => {
     const applicationStylesheet = "https://www.gov.uk/assets/static/application-2ff816c36affa8947857ec173cb86a848ca6559ef267a9245df61d2aa326f2df.css"
     const collectionsStylesheet = "https://www.gov.uk/assets/collections/application-07cc5dfda62258efd4b102a4e531e8d833f12007897649b6ef1045f9848e2d08.css"
     const frontendStylesheet = "https://www.gov.uk/assets/government-frontend/application-814f34c2ef20f2022c151c65111732a389100dc06a253a0b5464231377c60211.css"
+    const jsScriptFrontend = "https://www.gov.uk/assets/frontend/application-a8996011c81a73c3c09019a5b64dc0fcaecb308f2dd54977e29eaf0044eb6049.js"
+    const jsScriptStatic = "https://www.gov.uk/assets/static/application-9808e848693069a7918d9f48f225ee3df0e105b8a29fcdbf1733103f90f0e751.js"
       return renderToStaticMarkup(
           <html>
             <head>
                <link rel="stylesheet" href={applicationStylesheet} media="all"/>
                <link rel="stylesheet" href={collectionsStylesheet} media="all"/>
                <link rel="stylesheet" href={frontendStylesheet} media="all"/>
-               <script src="app/assets/javascripts/layout-super-navigation-header.js"></script>
+               <script src={jsScriptFrontend}></script>
+               <script src={jsScriptStatic}></script>
              </head>
              <body className="gem-c-layout-for-public govuk-template__body">
               { header() }
